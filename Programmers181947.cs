93bf46ab076ed0d9f3cab4f862cefa0145b4fcbb
LederyCore <lesb060500@gmail.com>
2025-09-18 16:34:46 +0900

    덧셈식 출력하기

---
+++ Programmers181947.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+
+
+public class Programmers181947
+{
+    public void Solution()
+    {
+        string[] str = Console.ReadLine().Split(' ');
+
+        int a = int.Parse(str[0]);
+        int b = int.Parse(str[1]);
+
+
+        Console.WriteLine(str[0] + " + " + str[1] + " = " + (a + b));
+    }
+}
