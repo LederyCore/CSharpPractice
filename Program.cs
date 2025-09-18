63180f88ba3535b70be36a317099b934a626948e
LederyCore <lesb060500@gmail.com>
2025-09-18 16:48:12 +0900

    문자열 붙여서 출력하기

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ public class Program
 {
     public static void Main()
     {
+        string[] s  = Console.ReadLine().Split(' ');
 
+        Console.WriteLine(s[0] + s[1]);
     }
 }
---
+++ Programmers181946﻿.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+public class Programmers181946
+{
+    
+}
