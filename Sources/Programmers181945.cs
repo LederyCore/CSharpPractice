83edcd8d392db27ea7a7a0aa2e840eb340519190
LederyCore <lesb060500@gmail.com>
2025-09-29 17:25:49 +0900

    문자열 돌리기

---
+++ Sources/Programmers181945.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+public class Programmers181945
+{
+
+
+    public void Solution()
+    {
+        string str = Console.ReadLine();
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            Console.WriteLine(str[i]);
+        }
+    }
+}
