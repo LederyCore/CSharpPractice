0a04412ece654375c38d6835059444b62fe0195e
LederyCore <lesb060500@gmail.com>
2025-10-01 10:46:31 +0900

    문자 리스트를 문자열로 변환하기, 문자열 곱하기

---
+++ Sources/Programmers181940.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+
+
+public class Programmers181940
+{
+    public string Solution(string[] my_string, int k)
+    {
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < k; i++)
+        {
+            sb.Append(my_string);
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ Sources/Programmers181941.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+
+using System.Text;
+
+public class Programmers181941
+{
+
+
+    public string Solution(char[] arr)
+    {
+        var sb = new StringBuilder();
+
+        foreach(var c in arr )
+        {
+            sb.Append(c);
+        }
+
+        return sb.ToString();
+    }
+}
